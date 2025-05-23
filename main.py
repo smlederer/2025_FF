3b2147e655132d40c02f36f8fee3699430a50fe9
smlederer <34457857+smlederer@users.noreply.github.com>
2025-05-23 12:33:50 -0400

    updated Trophy Case

--- main.py
+++ main.py
@@ -25,55 +25,36 @@ def get(manager_name:str):
     page = (Title(manager_name + ' - Greenwood Games'),
         #navigation
         Main(
-        Nav(Ul(Li(Select(*[Option(manager_name,selected=True)]+[Option(i) for i in managers if i != manager_name]))),
-            Ul(Li(Button('HOME')),
-               Li(Button('HALL OF FAME')),
-               Li(Button('MANAGERS')),
-               Li(Button('LEAGUE HISTORY')))
+        Nav(Ul(Li(H1(manager_name))),
+            Ul(Li(A('HOME',role = 'button')),
+               Li(A('MANAGERS',role = 'button')),
+               Li(A('LEAGUE HISTORY',role = 'button')),
+               Li(A('HALL OF FAME',role = 'button')))
             
             ),
         
         #Trophy Case
         Div(H4('TROPHY CASE')),
-        Div(*[Card('🥇',footer='2021',style='width:fit-content;text-align:center;margin:5px;') for i in range(4)],_class='box',style='display:flex;justify-content:center;'),
+        Div(*[Card(Div(dbu.trophy_converter(i['trophy']),style='font-size:3vw'),
+                   header=Button(B(f"{i['season']}"),_class='contrast'),
+                   style='width:fit-content;text-align:center;margin:5px;') for _,i in data.trophy_case.iterrows() if i['trophy'] != 'Null']
+                   ,_class='box',style='display:flex;justify-content:center;height:175px;'),
         #Div('tmp',id='trophy_case',style = 'width:fill;height:100px;background-color:#3e1c00;color:black;'),
 
-        Div(H4('RESULTS BY SEASON V2')),
+        Div(H4('RESULTS BY SEASON'+f" | {data.total_wins}W - {data.total_losses}L")),
         #Results By Season
         Div(*[Card(H4(f"{i[1]['wins']}-{i[1]['losses']}")
-                     ,header=Button(B(f"{i[0]}"),_class='contrast')
-                     ,footer=f"{'3rd'}"
+                     ,header=Button(B(f"{i[1]['season']}"),_class='contrast')
+                     ,footer=f"{dbu.ordinal(i[1]['result'])}"
                      ,style='width:100px;margin:5px;')
-                  for i in data.results_by_season.items()],_class='box',style='display:flex;padding:5px;justify-content:center;'),
-
-
-        # Div(H4('RESULTS BY SEASON V1')),
-        # #Reuslts by season v1
-        # Nav(
-        #     Ul(
-        #         Li(
-        #         Div(*[Button(B(f"{i[0]}"),f" | {i[1]['wins']}-{i[1]['losses']}",
-        #              hx_post=f"/managers/{manager_name}/results_by_season/{i[0]}",
-        #              hx_target="#results_graph",
-        #              hx_swap='innerHTML',
-        #              id=f'results_by_season_button',
-        #              _class = 'contrast',
-        #              style = 'font-size:min(1.5vw,32px);') 
-        #              for i in data.results_by_season.items()],style = 'overflow-x:auto;white-space:nowrap;')),
-        
-        
-        # Li(Button('Go To Season',_class='outline'))
-        # )),
-
-
-        #Div(data.results,id='results_graph',style = 'width:fill;height:300px;background-color:white;color:black;'),
+                  for i in data.results_by_season.items()],_class='box',style='display:flex;padding:5px;justify-content:center;text-align:center;'),
 
         #Manager Head to Head
         Div(H4('MANAGER HEAD TO HEAD')),
-        Div(*[Card(Div(A(i[0],_class='secondary',href='/managers/'+str(i[0])),style='text-align:center;font-size:1.5vw;',role='button',_class='outline')
-                   ,footer=Div(Div(f'{i[1]["wins"]}-{i[1]["losses"]}',style='text-align:center;font-size:2vw;'),
-                            Div(f'{("W"*i[1]["wins"]+"L"*i[1]["losses"]).replace("W","🟩").replace("L","🟥")}',
-                                style='text-align:start;font-size:1.5vw'),_class='grid',style='grid-template-columns:1fr 3fr')) 
+        Div(*[Card(Div(B(f'{i[1]["wins"]}-{i[1]["losses"]}'),style='text-align:center;font-size:24px;'),
+                   header=Div(B(A(i[0],_class='secondary',href='/managers/'+str(i[0]))),style='text-align:center;font-size:clamp(1rem, 1vw, 2rem)',role='button',_class='outline'),
+                   footer= Div(f'{(i[1]["lineage"]).replace("1","🟩").replace("0","🟥")}',
+                                style='text-align:start;font-size:max(1rem,1vw)'))
                    for i in data.results_head_to_head.items()]
                    ,_class='grid'
                    ,style='grid-template-columns:1fr 1fr 1fr 1fr'),
@@ -86,16 +67,16 @@ def get(manager_name:str):
 
         #Favorite Players
         Div(H4('FAVORITE PLAYERS')),
-        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=i['name'],footer=f'{i["tot"]} Starts') for _,i in data.favorite_players.iterrows()],_class='grid',style='overflow-x:auto;'),
+        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=B(i['name']),footer=f'{i["tot"]} Starts',style='text-align:center;font-size:clamp(1rem, 1vw, 2rem)') for _,i in data.favorite_players.iterrows()],_class='grid',style='overflow-x:auto;'),
             
         #Best Starts
         Div(H4('BEST STARTS')),
-        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=i['name'],footer=f'{i["season"]}, Week {i["week"]}: {i["points"]}') for _,i in data.best_starts.iterrows()],_class='grid',style='overflow-x:auto;'),
+        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=B(i['name']),footer=f'{i["season"]}, Week {i["week"]}: {i["points"]}',style='text-align:center;font-size:clamp(1rem, 1vw, 2rem)') for _,i in data.best_starts.iterrows()],_class='grid',style='overflow-x:auto;'),
 
 
         #Oops I should have Started
         Div(H4('OOPS SHOULD HAVE STARTED...')),
-        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=i['name'],footer=f'{i["season"]}, Week {i["week"]}: {i["points"]}') for _,i in data.oops.iterrows()],_class='grid',style='overflow-x:auto;'),
+        Div(*[Card(Img(src=f'https://a.espncdn.com/i/headshots/nfl/players/full/{int(i["espn_id"])}.png'),header=B(i['name']),footer=f'{i["season"]}, Week {i["week"]}: {i["points"]}',style='text-align:center;font-size:clamp(1rem, 1vw, 2rem)') for _,i in data.oops.iterrows()],_class='grid',style='overflow-x:auto;'),
 
 
 
@@ -113,33 +94,5 @@ def get(manager_name:str):
     return page
 
 
-@rt('/managers/{manager_name}/results_by_season/{season}')
-def post(manager_name:str,season:int):
-    
-    return 
-
-
-@rt('/managers/{player_name}/head_to_head/{name}')
-def post(player_name:str,name:str):
-    print(player_name + ' ' + name)
-    #return player_name + ' ' + manager
-
-
-
-
 if __name__ == '__main__':
     serve(port=6429)
-
-
-
-        #     Div(Nav(
-        #         Ul(*[Li(
-        #             Button(o.upper(),hx_post=f'/navlist/{o}',hx_target = '#content',hx_swap = 'innerHTML',style=f.button_style())
-        #             ) for o in ['Managers','League History']],Li(A(Button('Hall of Fame'.upper(),style=f.button_style()),href='/hof')))
-        #             )) ,style='width:80%;margin-left:10%;display:flex;justify-content:center;')
-        #     ,Div(P(f'''{f.get_tzu().upper()}''',
-        #            style='text-align:center;font-size:24px;font-family: "Times New Roman", Times, serif;font-style: italic;'),
-        #            P(NotStr(''' Sun Hoe Tzu, <i><a href='https://www.amazon.com/Fantasy-Football-Dummies-Martin-Signore/dp/0470125071'>The Art of Fantasy Football</a></i>'''),
-        #              style='text-align:center;font-family: "Times New Roman", Times, serif;')
-        #          ,style='width:80%;margin-left:10%;padding:10px 0px 10px 10px;border-radius:20px;',id='content')
-        # )
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -15,6 +15,20 @@ def get_list_of_managers():
     return data
 
 
+#helper functions
+
+
+def trophy_converter(placement):
+    conv = {0:'🐴',1:'🥇',2:'🥈',3:'🥉'}
+    if int(placement) in conv.keys():
+        return conv[int(placement)]
+    
+def ordinal(n):
+    if 10 <= n % 100 <= 20:
+        suffix = 'th'
+    else:
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
+    return f"{n}{suffix}"
 
 #User Metrics
 
@@ -27,22 +41,48 @@ def username_to_id_info(username:str): #->dict(list,pd.DataFrame)
     return {'user_ids':user_id,'roster_ids':roster_id}
 
 
-
-
 class user_info():
     def __init__(self,username):
         tmp_info = username_to_id_info(username)
         self.user_ids = tmp_info['user_ids']
         self.roster_ids = tmp_info['roster_ids']
 
+        self.trophy_case = self.get_final_standings()
+
         self.results = self.results_history()
         self.results_by_season = self.results_by_season_helper(self.results)
         self.results_head_to_head = self.head_to_head_helper(self.results)
+        self.total_wins, self.total_losses = self.total_helper(self.results)
 
         self.player_history = self.player_history_raw()
         self.favorite_players = self.favorite_player_helper(self.player_history)
         self.best_starts = self.best_starts_helper(self.player_history)
         self.oops = self.oops_should_have_started_helper(self.player_history)
+
+
+    #trophy case
+
+    def get_final_standings(self):
+        query = f'''
+                with tmp as (
+                select fr.season,u.user_id,u.username, fr.result, 
+                case when fr.result = max(fr.result) over (partition by fr.season) then 1 else 0 end as the_megan
+                from final_results fr
+                join users u on u.roster_id = fr.roster_id and u.league_id = fr.league_id
+                where true
+                group by 1,2,3
+                order by fr.season desc, result asc)
+
+                select *
+                from tmp
+                where user_id in ({",".join(self.user_ids)})
+                '''
+        data = cnx_and_query(query)
+        data = data[['season','result','the_megan']]
+        data['trophy'] = data.apply(lambda x: 0 if x['the_megan']==1 else x['result'],axis=1) #0 = megan, 1,2,3 = respective
+        data['trophy'] = data['trophy'].apply(lambda x: x if x <4 else 'Null')
+        return data
+
 
     #results and head to head data
 
@@ -68,12 +108,18 @@ class user_info():
 
     def results_by_season_helper(self,results_history:pd.DataFrame):
         agg = results_history.groupby('season')['win_flag'].agg(wins='sum',losses=lambda x: (x==0).sum())
-        return agg.sort_index(ascending=False).to_dict(orient='index')
+        agg = agg.merge(self.trophy_case[['season','result','trophy']], left_index=True, right_on='season')
+        return agg.sort_values('season',ascending=False).to_dict(orient='index')
     
     def head_to_head_helper(self,results_history:pd.DataFrame):
-        agg = results_history.groupby('username_challenger')['win_flag'].agg(wins='sum',losses=lambda x: (x==0).sum())
+        agg = results_history.groupby('username_challenger')['win_flag'].agg(wins='sum',losses=lambda x: (x==0).sum(),lineage=lambda x: ''.join(x.astype(str)))
         return agg.sort_index(ascending=True).to_dict(orient='index')
 
+    def total_helper(self,results_history:pd.DataFrame):
+        agg = results_history.groupby('user_id_root')['win_flag'].agg(wins='sum',losses=lambda x: (x==0).sum())
+        wins = sum(agg['wins'])
+        losses = sum(agg['losses'])
+        return wins,losses
 
     #player data - most starts, bench warmer game, 
 
@@ -115,10 +161,11 @@ class user_info():
     def oops_should_have_started_helper(self,player_history:pd.DataFrame):
         data = player_history[player_history['is_starter']==0][['name','espn_id','week','season','points']]
         return data.sort_values('points',ascending=False).head(5)
+    
 
 if __name__ == '__main__':
     u = user_info('smlederer')
     print(u.results_by_season)
     print(u.results_head_to_head)
-    print(u.player_history)
-    print(u.favorite_players)
+    print(u.trophy_case)
+    print(len(u.trophy_case))
